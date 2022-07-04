2231dd51081e64ae140463810c943ab5c2aa7c71
gokayari <gokayari@gmail.com>
2022-07-04 15:28:16 +0200

    CukesRunner.java

--- src/test/java/com/fidexio/runners/CukesRunner.java
+++ src/test/java/com/fidexio/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/fidexio/step_definitions",
         dryRun = false,
-        tags = "@FIDEX-390",
+        tags = "@FIDEX-423",
         publish = true
 )
 
